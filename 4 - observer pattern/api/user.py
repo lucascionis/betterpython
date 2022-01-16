ab8db8c016ff0bccc443443740a26bccb70402f3
Luca Scionis <lucascionis@tiscali.it>
2022-01-16 18:32:20 +0100

    Modified 'user.py' and 'observer-before.py'
    Applied changes to use new EventSystem and EventListener(s)

--- 4 - observer pattern/api/user.py
+++ 4 - observer pattern/api/user.py
@@ -1,13 +1,13 @@
-from lib.email import send_email
 from lib.db import create_user, find_user
-from lib.log import log
-from lib.slack import post_slack_message
 from lib.stringtools import get_random_string
+
+from .event import event_system
 
 def register_new_user(name: str, password: str, email: str):
     # create an entry in the database
     user = create_user(name, password, email)
     
+    """
     # post a Slack message to sales department
     post_slack_message("sales",
         f"{user.name} has registered with email address {user.email}. Please spam this person incessantly.")
@@ -19,6 +19,8 @@ def register_new_user(name: str, password: str, email: str):
 
     # write server log
     log(f"User registered with email address {user.email}")
+    """
+    event_system.trigger_event('user_registered', user)
 
 def password_forgotten(email: str):
     # retrieve the user
@@ -27,9 +29,13 @@ def password_forgotten(email: str):
     # generate a password reset code
     user.reset_code = get_random_string(16)
 
+    """
     # send a password reset message
     send_email(user.name, user.email, "Reset your password",
         f"To reset your password, use this very secure code: {user.reset_code}.\nRegards, The DevNotes team")
 
     # write server log
     log(f"User with email address {user.email} requested a password reset")
+    """
+
+    event_system.trigger_event('password_forgotten', user)
--- 4 - observer pattern/observer-before.py
+++ 4 - observer pattern/observer-before.py
@@ -1,11 +1,25 @@
 from api.user import register_new_user, password_forgotten
 from api.plan import upgrade_plan
 
-# register a new user
-register_new_user("Arjan", "BestPasswordEva", "hi@arjanegges.com")
+from api.email_listener import setup_email_listeners
+from api.log_listener import setup_log_listeners
+from api.slack_listener import setup_slack_listeners
 
-# send a password reset message
-password_forgotten("hi@arjanegges.com")
+def main():
+    # setup all listeners
+    setup_email_listeners()
+    setup_log_listeners()
+    setup_slack_listeners()
 
-# upgrade the plan
-upgrade_plan("hi@arjanegges.com")
+    # register a new user
+    register_new_user("Arjan", "BestPasswordEva", "hi@arjanegges.com")
+
+    # send a password reset message
+    password_forgotten("hi@arjanegges.com")
+
+    # upgrade the plan
+    #upgrade_plan("hi@arjanegges.com")
+
+
+if __name__ == '__main__':
+    main()
